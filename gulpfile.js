7c26bd404a871748653022ad0af57dda01291fd7
Sergey Razdolyansky <grafcorp01@gmail.com>
2018-03-16 12:03:46 +0300

    Add PUG task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,14 @@
 'use strict';
 
-var gulp = require('gulp');
-var gp = require('gulp-load-plugins')();
+global.$ = {
+  gulp : require('gulp'),
+    gp : require('gulp-load-plugins')()
+};
 
-gulp.task('sass', function () {
-   return gulp.src('./source/style/app.scss')
-       .pipe(gp.sourcemaps.init())
-       .pipe(gp.sass())
-       .on('errors', gp.notify.onError({
-           title: 'Style'
-       }))
-       .pipe(gp.autoprefixer({
-           browsers: [
-               'last 3 version',
-               '> 1%',
-               'ie 8',
-               'ie 9',
-               'Opera 12.1'
-           ]
-       }))
-       .pipe(gp.sourcemaps.write())
-       .pipe(gulp.dest('./build/assets/css'))
-});
+
+require('./gulp/tasks/sass');
+require('./gulp/tasks/pug');
+
+$.gulp.task('sass');
+$.gulp.task('pug');
+
